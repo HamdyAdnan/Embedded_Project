97da1f4cf883b1227ba6fd6e0d7b6139ef277287
HamdyAdnan <91385775+HamdyAdnan@users.noreply.github.com>
2023-03-22 16:26:31 +0200

    Update ADC_prg.c

--- ARM/MCAL/ADC/ADC_prg.c
+++ ARM/MCAL/ADC/ADC_prg.c
@@ -1,103 +1,125 @@
 /*
  * ADC_prg.c
  *
- *  Created on: Jan 8, 2015
- *      Author: DELL
+ *  Created on: Mar 9, 2023
+ *      Author: dell
  */
-#include "avr/interrupt.h"
-#include "../../LIB/STD_TYPES.h"
-#include "../../LIB/BIT_MATH.h"
-#include "ADC_pvt.h"
+#include "_delay_ms.h"
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+#include "ADC_priv.h"
 #include "ADC_config.h"
-#include "ADC_int.h"
+#include "ADC_init.h"
 
-volatile u16 Glob_u16ADCReading = 0;
-void (* ptr_callback)(void);
-void M_ADC_void_init		(prescaler_t Copy_enuPrescaler , vref_t Copy_enuRefVoltage)
+volatile u16 analog_data =0;
+volatile u16 counter=0;
+
+void (* M_ADC1_void_callback)(void);
+
+void M_ADC_void_Init(void)
 {
-	// Set Prescaler
-	ADCSRA_REG &= ADC_PRESCALER_MASK;
-	ADCSRA_REG |= Copy_enuPrescaler;
-	/*
-	// disable Interrupt
-	CLR_BIT(ADCSRA_REG, ADC_ADCSRA_ADIE_BIT);
-	*/
-	// Enable Interrupt
-	SET_BIT(ADCSRA_REG, ADC_ADCSRA_ADIE_BIT);
+	// ON ADC -> ADON -> 1
+	SET_BIT(ADC1->ADC_CR2 , 0);
+	_delay_ms(20);
 
-	// Config Right Adjust
-	CLR_BIT(ADMUX_REG, ADC_ADMUX_ADLAR_BIT);
+	// Set External event select for regular group -> SWSTART -> 111
+	SET_BIT(ADC1->ADC_CR2 , 19);
+	SET_BIT(ADC1->ADC_CR2 , 17);
+	SET_BIT(ADC1->ADC_CR2 , 18);
 
-	/*
-	// disable auto trigger
-	SET_BIT(ADCSRA_REG, ADC_ADCSRA_ADATE_BIT);
-	*/
+	// Set External trigger conversion mode for regular channels
+	SET_BIT(ADC1->ADC_CR2 , 20);
+	// Set  Data alignment -> Right Alignment -> ALIGN > 0
+	CLR_BIT(ADC1->ADC_CR2 , 11);
+	// Set Single conversion mode -> CONT -> 0
+	CLR_BIT(ADC1->ADC_CR2 , 1);
+	// Set Channel x Sample time selection -> Default -> 1.5 Cycle
 
-	// Enable auto trigger
-	SET_BIT(ADCSRA_REG, ADC_ADCSRA_ADATE_BIT);
+	// Set Number of Conversion in Regular Mode -> Default -> 1
 
-	// Select Reference
-	ADMUX_REG &= ADC_VREFF_MASK;
-	ADMUX_REG |= Copy_enuRefVoltage << ADC_VREFF_SHIFT;
 
-	// Enable ADC
-	SET_BIT(ADCSRA_REG , ADC_ADCSRA_ADEN_BIT);
-
-	// Start Conversion
-	ADCSRA_REG |=1 << ADC_ADCSRA_ADSC_BIT;
+	// Start Calibration -> CAL -> 1
+	SET_BIT(ADC1 -> ADC_CR2 , 2);
+	while(GET_BIT(ADC1->ADC_CR2 , 2) != 0);
 }
-u16 M_ADC_void_read			(channel_t   Copy_enuChannel)
+
+u16	M_ADC_u16_read	(Channel_ID Copy_enuChannel)
 {
-	u16 Local_u16Reading = 0;
-	// Select Channel
-	ADMUX_REG &= ADC_MUX_MASK;
-	ADMUX_REG |= ADC_CHANNEL;
-	// Start Conversion
-	ADCSRA_REG |=1 << ADC_ADCSRA_ADSC_BIT;
-	// polling wait until conversion is done
-	while(GET_BIT(ADCSRA_REG, ADC_ADCSRA_ADSC_BIT) == 1);
-	// return data
-	return ADCL_REG;
-}
-void M_ADC_CallBack			(void (*ptr_fun)(void))
-{
-	if(ptr_fun != NULL)
+	u32 Local_u32Reading;
+	// Set Channel
+	if(Copy_enuChannel <= CHANNEL_9)
 	{
-		ptr_callback = ptr_fun;
-	}
-}
 
-ISR(ADC_vect)
-{
-	Glob_u16ADCReading = ADCL_REG;
-	if(ptr_callback != NULL)
-	{
-		ptr_callback();
+		//ADC->SQR3 &= 0xfffffff0; // Clear 0 -> 4 bits
+		//ADC->SQR3 |= Copy_enuChannel;
+		CLR_BIT(ADC1->ADC_SQR3 , 0);
+		CLR_BIT(ADC1->ADC_SQR3 , 1);
+		CLR_BIT(ADC1->ADC_SQR3 , 2);
+		CLR_BIT(ADC1->ADC_SQR3 , 3);
+		CLR_BIT(ADC1->ADC_SQR3 , 4);
+
 	}
-}
+
+	// Start conversion of regular channels -> SWSTART -> 1
+	SET_BIT(ADC1->ADC_CR2 , 22);
+	// Wait until the end of conversion -> SWSTART -> 0
+	//while(GET_BIT(ADC->CR2 , 22) == 1);
+
+	while(GET_BIT(ADC1->ADC_SR , 1) != 1);
 
 /*
-void M_ADC_void_init		(void)
-{
-	// Select Reference
-	ADMUX_REG &= 0b00111111;
-	ADMUX_REG |= 1<<ADC_ADMUX_REFS0_BIT;
-	// Select Channel
-	ADMUX_REG &= ADC_MUX_MASK;
-	ADMUX_REG |= ADC_CHANNEL;
-	// Set Prescaler
-	ADCSRA_REG &= ADC_PRESCALER_MASK;
-	ADCSRA_REG |= ADC_PRESCALER;
-	// Enable ADC
-	SET_BIT(ADCSRA_REG , ADC_ADCSRA_ADEN_BIT);
-}
-void M_ADC_void_read		(u16 * Copy_pu16Value)
-{
-	// Start Conversion
-	ADCSRA_REG |=1 << ADC_ADCSRA_ADSC_BIT;
-	// Stay until the End of Conversion
-	while(GET_BIT(ADCSRA_REG, ADC_ADCSRA_ADSC_BIT) == 1);
-	* Copy_pu16Value = ADCL_REG;
-}
+	// Start conversion of regular channels -> SWSTART -> 1
+	SET_BIT(ADC->CR2 , 0);
+	// Wait until the end of conversion -> SWSTART -> 0
+	while(GET_BIT(ADC->SR , 1) != 1);
 */
+	// Get data -> 0 -> 15
+//	Local_u32Reading = (ADC->DR) & 0x0000ffff;
+	// Return Data
+	return ADC1->ADC_DR;
+}
+
+void ADC1_2_IRQHandler(void)
+{
+
+
+		//check if the End of conversion is completed
+		if (ADC1->ADC_SR & (1<<1))
+		{
+			analog_data = ADC1->ADC_DR ;
+		//	ADC1->ADC_SR &=~(1<<1);
+		//	ADC1->ADC_CR2 |=(1<<22); /*Start conversion of regular channels*/
+
+		}
+
+		else
+		{
+			//counter--;
+			//conversion not happened
+		}
+
+
+		counter++;
+	//MADC1_callback();
+
+
+}
+
+//take care if there is an interrupt fired after firing ADC_handeler callback will jump to garbage location
+// so itis recomendded to make call_back function before initialization in main()
+
+void M_ADC1_void_set_callback(void (*ptr) (void))
+{
+
+ M_ADC1_void_callback = ptr;
+
+}
+
+
+
+u16 M_ADC1_u16_getdata(void)
+{
+
+	return analog_data;
+}
 
